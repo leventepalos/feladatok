e41432117ec4768eb9abe81059883884efdb2f41
leventepalos <82084813+leventepalos@users.noreply.github.com>
2023-11-24 06:48:48 +0100

    Add files via upload

---
+++ szammmmmmmmmm/szammmmmmmmmm/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Random random = new Random();
+        int gondoltSzam = random.Next(1, 101);
+
+        Console.WriteLine("Gondoltam egy számra 1 és 100 között. Próbáld meg kitalálni!");
+
+        int tipp;
+        bool helyes = false;
+
+        while (!helyes)
+        {
+            Console.Write("Tippelj egy számot: ");
+
+           
+            if (int.TryParse(Console.ReadLine(), out tipp))
+            {
+                if (tipp < 1 || tipp > 100)
+                {
+                    Console.WriteLine("A számnak 1 és 100 között kell lennie. Próbáld újra!");
+                }
+                else
+                {
+                    
+                    if (tipp < gondoltSzam)
+                    {
+                        Console.WriteLine("Túl kicsi próbáld újra.");
+                    }
+                    else if (tipp > gondoltSzam)
+                    {
+                        Console.WriteLine("Túl nagy próbáld újra.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Gratulálok eltaláltad a számot ({gondoltSzam}).");
+                        helyes = true;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Érvénytelen bemenet. Kérlek írj be egy érvényes számot.");
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
+
---
+++ szokereső/szokereső/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace szokereső
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Szavak = new String[15];
+            string[] Tippre_valasz = new String[6];
+            bool Leall = false;
+            string valasz;
+            int darab = 1;
+            int szamol = 0;
+
+            StreamReader Beolvas = new StreamReader("szavak.txt", Encoding.Default);
+
+            for (int i = 0; i < 15; i++)
+            {
+                Szavak[i] = Beolvas.ReadLine();
+            }
+
+            Beolvas.Close();
+
+            Random Rnd = new Random();
+            int index = Rnd.Next(0, 15);
+
+            string Rejtett_szo = Szavak[index];
+            Console.WriteLine(Rejtett_szo);
+
+            Console.Write("Kérem a tippet: ");
+            valasz = Console.ReadLine();
+            if (valasz == "stop")
+            {
+                Leall = true;
+            }
+
+            while (Leall == false)
+            {
+                if (valasz == Rejtett_szo)
+                {
+                    Console.WriteLine("Az eredmény: {0}", Rejtett_szo);
+                    Console.WriteLine("\n{0} tippeléssel sikerült kitalálni.", darab);
+                    Leall = true;
+                }
+
+                for (int i = 0; i < 6; i++)
+                {
+                    string betu_valasz = valasz.Substring(i, 1);
+                    string betu_rejtett = Rejtett_szo.Substring(i, 1);
+
+                    if (betu_valasz == betu_rejtett)
+                    {
+                        Tippre_valasz[i] = betu_valasz;
+                    }
+                    else
+                    {
+                        Tippre_valasz[i] = ".";
+                    }
+                }
+
+                darab++;
+
+                for (int i = 0; i < 6; i++)
+                {
+                    if (Tippre_valasz[i] == Rejtett_szo.Substring(i, 1))
+                    {
+                        szamol++;
+                    }
+                }
+
+                if (szamol == 6)
+                {
+                    Console.WriteLine("Az eredmény: {0}", Rejtett_szo);
+                    Console.WriteLine("\n{0} tippeléssel sikerült kitalálni.", darab);
+                    Leall = true;
+                }
+                else
+                {
+                    Console.Write("Az eredmény: ");
+                    for (int i = 0; i < 6; i++)
+                    {
+                        Console.Write(Tippre_valasz[i]);
+                    }
+                    Console.Write("\n\nKérem a tippet: ");
+                    valasz = Console.ReadLine();
+                    if (valasz == "stop")
+                    {
+                        Leall = true;
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
